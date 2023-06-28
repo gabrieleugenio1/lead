4ad666fad22d8cafde29cbed44ce299b85c29841
Adyson Alves <adyson6@gmail.com>
2023-06-28 20:10:47 -0300

    Ajustes

--- src/database/conexao.js
+++ src/database/conexao.js
@@ -1,12 +1,12 @@
 const {Sequelize} = require('sequelize');
 const conexao = new Sequelize({
-    database: 'leads',
-    username: 'root',
-    password: '1234',
-    host: 'localhost',
-    port: '3306',
-    dialect: 'mysql',
-    timezone: '-03:00'
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    timezone: process.env.DB_TIMEZONE
 });
 
 module.exports = conexao;
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ const LeadsController = require('./controllers/LeadsController');
 const router = express.Router();
 
 
-router.get('/leads', LeadsController.listarTodos);
-router.get('/leads/:id', LeadsController.getLeadById);
-router.post('/leads', LeadsController.createLead);
-router.put('/leads/:id', LeadsController.updateLead);
-router.delete('/leads/:id', LeadsController.deleteLead);
+router
+.get('/leads', LeadsController.listarTodos)
+.get('/leads/:id', LeadsController.getLeadById)
+.post('/leads', LeadsController.createLead)
+.put('/leads/:id', LeadsController.updateLead)
+.delete('/leads/:id', LeadsController.deleteLead)
 
 module.exports = router;
